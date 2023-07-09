6d80f9a43d4b664a0361dd31462d715c39974e9c
MEGAN ODONOHOE <MEGANODT@gmail.com>
2023-07-09 19:54:32 +0000

    Amend bracket in Array

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const playerUpdates = document.getElementById("player-updates");
-const choices = ("rock", "paper", "scissors");
+const choices = ["rock", "paper", "scissors"];
 
 // Event listeners for the buttons
 
@@ -25,12 +25,12 @@ for (let button of buttons) {
 
 function playGame(playerChoice) {
 
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
     playerImage.alt = choices[playerChoice];
 
     let computerChoice = Math.floor(Math.random() * 3);
 
-    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.src = `assets/images/${choices[computerChoice]}.jpg`;
     computerImage.alt = choices[computerChoice];
 
     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
@@ -42,3 +42,30 @@ function playGame(playerChoice) {
  * Checks the winner of the game
  * Accepts two strings
 **/
+
+function checkWinner(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return "Draw!";
+    } else if (
+        (computerChoice === "rock" && playerChoice === "scissors") ||
+        (computerChoice === "paper" && playerChoice === "rock") ||
+        (computerChoice === "scissors" && playerChoice === "paper")
+    ) {
+        return "computer";
+    } else {
+        return "player";
+    }
+}
+
+function updateScores(result) {
+    if (result === "player") {
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+        playerUpdates.textContent = "You win!";
+    } else if (result === "computer") {
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+        playerUpdates.textContent = "Computer wins!";
+    } else {
+        playerUpdates.textContent = "Draw!";
+    }
+}
+
