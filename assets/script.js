da1d95a798d07a1b5a6a27d421a3857278f8aebc
MEGAN ODONOHOE <MEGANODT@gmail.com>
2023-07-09 17:56:08 +0000

    Define constants

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Constants
+
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score")
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const playerUpdates = document.getElementById("player-updates");
+const choices = ("rock!", "paper!", "scissors!");
+
