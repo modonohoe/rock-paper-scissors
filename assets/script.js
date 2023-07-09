9e71569cdf579943292426be7ba40a8feb5ea7ab
MEGAN ODONOHOE <MEGANODT@gmail.com>
2023-07-09 18:09:01 +0000

    Define main game function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,44 @@
 // Constants
 
 const buttons = document.getElementsByClassName("control");
-const playerScore = document.getElementById("player-score")
+const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const playerUpdates = document.getElementById("player-updates");
-const choices = ("rock!", "paper!", "scissors!");
+const choices = ("rock", "paper", "scissors");
 
+// Event listeners for the buttons
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+
+}
+
+/**
+ * Main game function. Accepts one parameter;
+ * the data-choice of the button selected
+ */
+
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
+
+/**
+ * Checks the winner of the game
+ * Accepts two strings
+**/
